7387e58d4a33890fa35420d47e934bbe07c4eb8e
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-05-06 15:06:59 +0530

    fix: Added cors configuration

--- src/app.js
+++ src/app.js
@@ -9,16 +9,25 @@ app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const corsOptionForCredentials = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
+  credentials: true,
+};
+
+app.use(cors(corsOptionForCredentials));
+
 // Default route
-app.get('/', cors(), async (req, res) => {
+app.get('/', async (req, res) => {
   res.status(200).json({ message: 'Railway-Server API' });
 });
 
 app.use('/user', routes.userRoutes);
 app.use('/admin', routes.adminRoutes);
 app.use('/train', routes.trainRoutes);
+
 // Not found route
-app.get('*', cors(), (req, res) => {
+app.get('*', (req, res) => {
   return res.status(404).json({ message: 'API URL is not valid' });
 });
 
