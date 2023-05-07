dd47e076729277c86b63262123380f4c6a249fef
AlgorithmChopda <vinaychopda4@gmail.com>
2023-05-08 01:27:47 +0530

    feat: added protected route to admin

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -2,12 +2,18 @@ import { Router } from 'express';
 import * as adminController from '../controller/admin.controller.js';
 import { createRequire } from 'module';
 import * as trainController from '../controller/train.controller.js';
+import { isAdminAuth } from '../middleware/isAdminAuth.js';
 
 const router = Router();
 const require = createRequire(import.meta.url);
 const multer = require('multer');
 
 const upload = multer({ dest: 'uploads/' });
-router.post('/csvData', upload.single('file'), trainController.csvData);
+router.post(
+  '/csvData',
+  isAdminAuth,
+  upload.single('file'),
+  trainController.csvData,
+);
 router.post('/login', adminController.login);
 export default router;
